bbd4f8b5ff480a3093100c3ae5ab0a9f5766c1a7
kris2tis <m.rezaei.coder@gmail.com>
2025-12-07 14:57:46 +0330

    Updating the profile page (adding the theme page to the page navigations)

--- src/app/(app)/profile/page.jsx
+++ src/app/(app)/profile/page.jsx
@@ -1,36 +1,43 @@
+import { ProfileIcon, ThemeIcon } from "@/shared/assets/icons/icons";
 import ExitAccountButton from "@/shared/componetns/common/exit-account-button";
 import Image from "next/image";
 import Link from "next/link";
 
+const profileNav = [
+  { name: "تغییر اطلاعات حساب", icon: ProfileIcon, href: "/account" },
+  { name: "تغییر پوسته", icon: ThemeIcon, href: "/theme" },
+];
+
 export default function Page() {
   return (
     <div className="flex flex-col gap-y-2.5">
-      <ProfileNavCard />
+      {profileNav.map((pN, index) => (
+        <ProfileNavCard key={index} {...pN} />
+      ))}
       <ExitAccountButton />
     </div>
   );
 }
 
-const ProfileNavCard = () => {
+const ProfileNavCard = ({ name, href, icon }) => {
+  const Icon = icon;
   return (
-    <Link href={"/account"}>
-    <div className="flex justify-between items-center bg-secondary-900 rounded-md px-3.5 py-2.5">
-      <div className="flex gap-x-2.5">
-        <div className="relative h-4 aspect-square">
-          <Image src={"/assets/icons/profile.svg"} fill alt="account icon" />
+    <Link href={href}>
+      <div className="flex justify-between items-center bg-secondary-900 rounded-md px-3.5 py-2.5">
+        <div className="flex gap-x-2.5">
+          <Icon iconColor="fill-secondary-100" />
+          <span className="text-[12px]">{name}</span>
         </div>
-        <span className="text-[12px]">تغییر اطلاعات حساب</span>
-      </div>
-      <div>
-        <div className="relative h-2.5 aspect-square">
-          <Image
-            src={"/assets/icons/left-short-arrow.svg"}
-            fill
-            alt="left short arrow icon"
-          />
+        <div>
+          <div className="relative h-2.5 aspect-square">
+            <Image
+              src={"/assets/icons/left-short-arrow.svg"}
+              fill
+              alt="left short arrow icon"
+            />
+          </div>
         </div>
       </div>
-    </div>
     </Link>
   );
 };
